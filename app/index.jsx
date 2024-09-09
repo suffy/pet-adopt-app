2ee77609d543ae3627de794ff9a15ef784693e25
suffy <suffy.yanuar@gmail.com>
2024-09-09 11:43:36 +0700

    first setup project with clerk

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Link href="/login">
+        <Text>Go to login screen</Text>
+      </Link>
+    </View>
+  );
+}
