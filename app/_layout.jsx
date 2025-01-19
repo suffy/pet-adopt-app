24d361fd6262d72d17d28c705bb244de2466e614
suffy <suffy.yanuar@gmail.com>
2025-01-19 10:23:26 +0700

    implement auth with clerk

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,26 @@
+import { tokenCache } from "@/cache";
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+  if (!publishableKey) {
+    throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
+  }
+
   useFonts({
     "outfit-regular": require("./../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
   });
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,9 +1,54 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import * as WebBrowser from "expo-web-browser";
+import React, { useEffect, useState } from "react";
 import Colors from "./../../constants/Colors";
+import { Link, Redirect } from "expo-router";
+import { SignedIn, useOAuth, useUser } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function LoginScreen() {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({
+          redirectUrl: Linking.createURL("/home", { scheme: "myapp" }),
+        });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        // setActive!({ session: createdSessionId })
+      } else {
+        // Use signIn or signUp returned from startOAuthFlow
+        // for next steps, such as MFA
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
+
   return (
     <View style={{ backgroundColor: Colors.WHITE, height: "100%" }}>
+      <SignedIn>
+        <Redirect href="/home" />
+      </SignedIn>
       <Image
         source={require("./../../assets/images/login.png")}
         style={{ width: "100%", height: 500 }}
@@ -16,7 +61,7 @@ export default function LoginScreen() {
             textAlign: "center",
           }}
         >
-          Ready to make an new friend ?
+          Ready to make an new friend
         </Text>
         <Text
           style={{
@@ -29,6 +74,7 @@ export default function LoginScreen() {
           Let's adopt the pet which you like and make there life happy again
         </Text>
         <TouchableOpacity
+          onPress={onPress}
           style={{
             padding: 14,
             // marginTop: 100,
